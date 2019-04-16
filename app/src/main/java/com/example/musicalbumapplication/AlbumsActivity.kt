5d423f313e7c72b87c3c88b06d44cbd6b18e0a3b
VladaNills <vladarub@yandex.ru>
2019-04-16 21:11:07 +0300

    fix album_item

--- app/src/main/java/com/example/musicalbumapplication/AlbumsActivity.kt
+++ app/src/main/java/com/example/musicalbumapplication/AlbumsActivity.kt
@@ -3,7 +3,7 @@ package com.example.musicalbumapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AlbumsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
